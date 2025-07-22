175739464b5239573642abde71d51e49a064df40
Alisa Lain <60989682+al1-ce@users.noreply.github.com>
2025-07-22 19:21:44 +0300

    Update custom.js

--- public/js/bangs/custom.js
+++ public/js/bangs/custom.js
@@ -8,17 +8,17 @@ bangs = [].concat([
     },
     {
         // doesnt matter
-        d: "https://web.telegram.org/a/",
+        d: "web.telegram.org/a/",
         b: "tg",
         u: "https://web.telegram.org/a/"
     },
     {
-        d: "https://duckduckgo.com/bangs",
+        d: "duckduckgo.com/bangs",
         b: "bangs",
         u: "https://duckduckgo.com/bangs?q={{{s}}}"
     },
     {
-        d: "https://www.discordapp.com/channels/@me",
+        d: "www.discordapp.com/channels/@me",
         b: "cord",
         u: "https://www.discordapp.com/channels/@me"
     }
