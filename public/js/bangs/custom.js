cb8f0589e02fb853c49afb936dab5e14ec77ffa1
Alisa Lain <60989682+al1-ce@users.noreply.github.com>
2025-07-22 15:41:13 +0300

    add cord and tg bangs

--- public/js/bangs/custom.js
+++ public/js/bangs/custom.js
@@ -6,4 +6,20 @@ bangs = [].concat([
         b: "dh",
         u: "https://html.duckduckgo.com/html/?q={{{s}}}",
     },
+    {
+        // doesnt matter
+        d: "https://web.telegram.org/a/",
+        b: "tg",
+        u: "https://web.telegram.org/a/"
+    },
+    {
+        d: "https://duckduckgo.com/bangs",
+        b: "bangs",
+        u: "https://duckduckgo.com/bangs?q={{{s}}}"
+    },
+    {
+        d: "https://www.discordapp.com/channels/@me",
+        b: "cord",
+        u: "https://www.discordapp.com/channels/@me"
+    }
 ], bangs);
